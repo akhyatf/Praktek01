ced5e7e4dae1f51de7734b4bc3a271ca1c560290
ahmadkusairi <kusairi9616@gmail.com>
2018-03-26 18:21:35 +0800

    pak ini instance object

--- src/praktek01/BuahhAksi.java
+++ src/praktek01/BuahhAksi.java
@@ -10,5 +10,10 @@ package praktek01;
  * @author Mamed
  */
 public class BuahhAksi {
-    
+    public static void main(String[] args) {
+        Buahh 1 = new Buahh();
+        Buahh 2 = new Buahh();
+        Buahh 3 = new Buahh();
+    }
+        
 }
