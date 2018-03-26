bf8f643adc2ea01d801e786276f50133cc0e8a12
ahmadkusairi <kusairi9616@gmail.com>
2018-03-26 18:13:55 +0800

    membuat class atribut dan behavior

--- src/praktek01/Buahh.java
+++ src/praktek01/Buahh.java
@@ -10,6 +10,20 @@ package praktek01;
  * @author Mamed
  */
 public class Buahh {
+ +       string jenis_buah;
+ +       string nama_buah;
+ +       string warna_buah;
+ +       string harga_buah;
+ +       string bentuk_buah;
     
-            
+
+void cetakInfo(){
+
+    system.out.println("jenis_buah  :"+jenis_buah);
+    system.out.println("nama_buah   :"+nama_buah);
+    system.out.println("warna_buah  :"+warna_buah);
+    system.out.println("harga_buah  :"+harga_buah);
+    system.out.println("bentuk_buah  :"+bentuk_buah);
     }
+}
+
